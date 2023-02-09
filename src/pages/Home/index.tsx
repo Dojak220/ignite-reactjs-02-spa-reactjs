496c832f5c836fa6c37f35fad0e167fae1614b61
Dorgival Rocha <dojak220@ufrn.edu.br>
2023-02-08 22:14:58 -0300

    Build Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,61 @@
+import { Play } from "phosphor-react";
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountDownButton,
+  TaskInput,
+} from "./styles";
+
 export function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            type="task"
+            placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
+
+          <label htmlFor="">durante</label>
+          <MinutesAmountInput
+            type="number"
+            name="minutesAmount"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountDownButton type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountDownButton>
+      </form>
+    </HomeContainer>
+  );
 }
